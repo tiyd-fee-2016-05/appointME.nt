531a5b477f20bc40f35017011b018175e61d0624
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-06-26 18:33:03 -0400

    Deleted weathertogether.js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,35 +1,20 @@
-$(document).ready(function(){
+var LScity = localStorage.getItem("city");
 
-  $('.save-app').submit(function(e){
-    e.preventDefault();
-    console.log("Submit works");
+console.log(LScity);
 
-    var a = $('input-city').val();
-    var b =a.split(" ");
-    var city = b[1];
-    var state = b[2];
-    var citycap = city.charAt(0).toUpperCase() + city.slice(1);
-
-    if (a === "@weather" + " " + city + " " + state || a === "@weather" + " " + city + "," + " " + state) {
-      console.log("weather!");
-      console.log(b, b);
-      // console.log(weather);
-
-      $.ajax({
-              url: "https://api.wunderground.com/api/35c9d364b4732721/forecast/q/NC/" + city + "/.json",
-              method: 'GET',
-              dataType: "JSON",
-              success: function(json) {
-                $('.weather').append({temp} + "@weather:");
-                $('.weather').append("<li class='chatcontent'>" + "The current forecast for " + citycap + " is: " + "<br>" + "<br>" + json.forecast.txt_forecast.forecastday[0].fcttext + "<br>" +  "<img class='weatherimg' src='" + json.forecast.txt_forecast.forecastday[0].icon_url + "'>"  +  "<img class='weatherimg' src='" + json.forecast.txt_forecast.forecastday[0].icon_url + "'>"  +  "<img class='weatherimg' src='" + json.forecast.txt_forecast.forecastday[0].icon_url + "'>"  + "<img class='weatherimg' src='" + json.forecast.txt_forecast.forecastday[0].icon_url + "'>" + "<img class='weatherimg' src='" + json.forecast.txt_forecast.forecastday[0].icon_url + "'>" + "</li>");
-
-                console.log(json);
-                console.log(json.forecast.txt_forecast.forecastday[0].icon_url );
-
-                $('input').val("");
-              }
-            });
-    }
- });
-
+$.ajax({
+  url: "https://api.wunderground.com/api/35c9d364b4732721/forecast/q/NC/" + LScity + "/.json",
+  method: "GET",
+  dataType: "JSON",
+  success: function(rhonda){
+    var temp = rhonda.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    console.log(temp);
+    var icon = rhonda.forecast.simpleforecast.forecastday[0].icon;
+    console.log(icon);
+    var img = rhonda.forecast.simpleforecast.forecastday[0].icon_url;
+    console.log(img);
+    // $(".weather").html(icon + " " + temp + "&#8457;").append('<img src="' + img + '"/>');
+    $(".weather").append('<img src="' + img + '"/>' + '<span>' + icon + ", " + temp + "&#8457;" + '</span>');
+    $(".left-side").append('<img src="' + img + '"/>');
+  }
 });
